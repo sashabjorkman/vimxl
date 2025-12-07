2e4d6f46f01a94bb04807fa80d333387ea26f342
Alexander B <ab3.github@eit.se>
2025-12-07 04:36:10 +0100

    (fix) added linewise text object

--- motionmodes.lua
+++ motionmodes.lua
@@ -1,10 +1,3 @@
-
-local MOTION_MODE_TEXT_OBJECT = {
-  is_linewise = false,
-  is_charwise = true,
-  is_text_object = true,
-}
-
 --When set selection will be clamped
 --to include every line that it passes. Including the newline character.
 --Mutually exclusive with CHARWISE.
@@ -21,6 +14,18 @@ local MOTION_MODE_CHARWISE = {
   is_text_object = false,
 }
 
+local MOTION_MODE_CHARWISE_TEXT_OBJECT = {
+  is_linewise = false,
+  is_charwise = true,
+  is_text_object = true,
+}
+
+local MOTION_MODE_LINEWISE_TEXT_OBJECT = {
+  is_linewise = true,
+  is_charwise = false,
+  is_text_object = true,
+}
+
 ---@alias vimxl.motion_mode {is_linewise: boolean, is_charwise: boolean, is_text_object: boolean}
 
 --A collection of bitmasks that define how different motions and text objects should be treated.
@@ -34,14 +39,14 @@ local vim_motion_modes = {
   ["vimxl-motion:nth-line-printable"] = MOTION_MODE_LINEWISE,
   ["vimxl-motion:end-or-line-no"] = MOTION_MODE_LINEWISE,
 
-  ["vimxl-motion:entire-current-line-or-more"] = MOTION_MODE_TEXT_OBJECT,
-  ["vimxl-motion:select-inner-word"] = MOTION_MODE_TEXT_OBJECT,
-  ["vimxl-motion:select-in-paragraph"] = MOTION_MODE_TEXT_OBJECT,
+  ["vimxl-motion:entire-current-line-or-more"] = MOTION_MODE_CHARWISE_TEXT_OBJECT,
+  ["vimxl-motion:select-inner-word"] = MOTION_MODE_CHARWISE_TEXT_OBJECT,
+  ["vimxl-motion:select-in-paragraph"] = MOTION_MODE_LINEWISE_TEXT_OBJECT,
 }
 
 return {
   motion_modes = vim_motion_modes,
   MOTION_MODE_CHARWISE = MOTION_MODE_CHARWISE,
   MOTION_MODE_LINEWISE = MOTION_MODE_LINEWISE,
-  MOTION_MODE_TEXT_OBJECT = MOTION_MODE_TEXT_OBJECT,
+  MOTION_MODE_CHARWISE_TEXT_OBJECT = MOTION_MODE_CHARWISE_TEXT_OBJECT,
 }
